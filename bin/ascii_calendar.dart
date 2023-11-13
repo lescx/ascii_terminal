833ff97a064fabbd7d7eca61da01ffffb6ccba69
Luca Etienne Schulz <git@les.cx>
2023-11-13 12:39:33 +0100

    Allow user input

--- bin/ascii_calendar.dart
+++ bin/ascii_calendar.dart
@@ -1,5 +1,29 @@
 import 'package:ascii_calendar/ascii_calendar.dart' as ascii_calendar;
 
 void main(List<String> arguments) {
-  ascii_calendar.printCalendar(8, 3);
+  if (arguments.length != 2) {
+    print(
+        'Bitte geben Sie genau zwei Argumente ein: Monat (1-12) und Startwochentag (1-7).');
+    return;
+  }
+
+  try {
+    int month = int.parse(arguments[0]);
+    int startWeekday = int.parse(arguments[1]);
+
+    // Überprüfen, ob die Werte im erwarteten Bereich liegen
+    if (month < 1 || month > 12) {
+      print('Der Monatswert muss zwischen 1 und 12 liegen.');
+      return;
+    }
+    if (startWeekday < 1 || startWeekday > 7) {
+      print('Der Startwochentag muss zwischen 1 (MO) und 7 (SO) liegen.');
+      return;
+    }
+
+    ascii_calendar.printCalendar(month, startWeekday);
+  } catch (e) {
+    print(
+        'Fehler bei der Verarbeitung der Argumente: Stellen Sie sicher, dass sowohl Monat als auch Startwochentag Zahlen sind.');
+  }
 }
