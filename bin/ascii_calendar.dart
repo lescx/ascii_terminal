5500d6b4fde2d304e176bf22b2eaac5b13a17b69
Luca Etienne Schulz <git@les.cx>
2023-11-13 10:35:44 +0100

    Initial commit

---
+++ bin/ascii_calendar.dart
@@ -0,0 +1,5 @@
+import 'package:ascii_calendar/ascii_calendar.dart' as ascii_calendar;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ascii_calendar.calculate()}!');
+}
---
+++ test/ascii_calendar_test.dart
@@ -0,0 +1,8 @@
+import 'package:ascii_calendar/ascii_calendar.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
