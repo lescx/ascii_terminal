37d824555ee2277dad77e70c0558f0f0ee4314df
Luca Etienne Schulz <git@les.cx>
2023-11-13 14:08:01 +0100

    Reformat lib

--- lib/ascii_calendar.dart
+++ lib/ascii_calendar.dart
@@ -1,11 +1,10 @@
-/*
-|MO|DI|MI|DO|FR|SA|SO|
-|  |  | 1| 2| 3| 4| 5|
-| 6| 7| 8| 9|10|11|12|
-|13|14|15|16|17|18|19|
-|20|21|22|23|24|25|26|
-|27|28|29|30|31|  |  |
-*/
+// Leap year is not taken into account
+List<int> daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+// In this case, 'MO' is index 0; ascii_terminal does not make assumptions on
+// which weekday is which index. The user specifies the start of the index
+// in printCalendar(int, int)
+List<String> weekdays = ['MO', 'DI', 'MI', 'DO', 'FR', 'SA', 'SO'];
 
 // Wrapper function for printCalender
 // Does not get called directly
@@ -60,11 +59,3 @@ String printCalendar(int month, int startWeekday) {
   
   return calendar;
 }
-
-// Leap year is not taken into account
-List<int> daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-// In this case, 'MO' is index 0; ascii_terminal does not make assumptions on
-// which weekday is which index. The user specifies the start of the index
-// in printCalendar(int, int)
-List<String> weekdays = ['MO', 'DI', 'MI', 'DO', 'FR', 'SA', 'SO'];
