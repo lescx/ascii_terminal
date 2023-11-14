7da576ba157156a55807c38562802ee5a5f7dd95
Luca Etienne Schulz <git@les.cx>
2023-11-14 11:39:26 +0100

    Format lib

--- lib/ascii_calendar.dart
+++ lib/ascii_calendar.dart
@@ -1,9 +1,6 @@
 // Leap year is not taken into account
 List<int> daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-// In this case, 'MO' is index 0; ascii_terminal does not make assumptions on
-// which weekday is which index. The user specifies the start of the index
-// in printCalendar(int, int)
 List<String> weekdays = ['MO', 'DI', 'MI', 'DO', 'FR', 'SA', 'SO'];
 
 // Wrapper function for printCalender
